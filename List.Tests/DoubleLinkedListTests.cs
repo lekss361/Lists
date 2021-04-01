5347e2f44cf1bfb6ecd33706da8a9c4403d9a3ac
Aleks <alexandro361@gmail.ru>
2021-04-01 16:10:52 +0300

    add metods

--- List.Tests/DoubleLinkedListTests.cs
+++ List.Tests/DoubleLinkedListTests.cs
@@ -5,74 +5,74 @@ using NUnit.Framework;
 
 namespace List.Tests
 {
-    class DoubleLinkedListTests
+  class DoubleLinkedListTests
+  {
+    [TestCase(5, new int[] { 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4, 5 })]
+    [TestCase(8, new int[] { 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4, 8 })]
+    [TestCase(7, new int[] { }, new int[] { 7 })]
+    public void AddTest(int value, int[] array, int[] expectedArray)
     {
-        [TestCase(5, new int[] { 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4, 5 })]
-        [TestCase(8, new int[] { 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4, 8 })]
-        [TestCase(7, new int[] { }, new int[] { 7 })]
-        public void AddTest(int value, int[] array, int[] expectedArray)
-        {
-            DoubleLinkedList actual = new DoubleLinkedList(array);
-            DoubleLinkedList expected = new DoubleLinkedList(expectedArray);
+      DoubleLinkedList actual = new DoubleLinkedList(array);
+      DoubleLinkedList expected = new DoubleLinkedList(expectedArray);
 
-            actual.Add(value);
+      actual.Add(value);
 
-            Assert.AreEqual(expected, actual);
-        }
+      Assert.AreEqual(expected, actual);
+    }
 
-        [TestCase(new int[] { 1, 2, 3 }, 2, new int[] { 2, 1, 2, 3 })]
-        [TestCase(new int[] { 1, 2, 3 }, 3, new int[] { 3, 1, 2, 3 })]
-        [TestCase(new int[] { }, 4, new int[] { 4 })]
-        public void AddByZeroIndexTest(int[] actualAr, int value, int[] arExpected)
-        {
-            DoubleLinkedList actual = new DoubleLinkedList(actualAr);
-            DoubleLinkedList expected = new DoubleLinkedList(arExpected);
+    [TestCase(new int[] { 1, 2, 3 }, 2, new int[] { 2, 1, 2, 3 })]
+    [TestCase(new int[] { 1, 2, 3 }, 3, new int[] { 3, 1, 2, 3 })]
+    [TestCase(new int[] { }, 4, new int[] { 4 })]
+    public void AddByZeroIndexTest(int[] actualAr, int value, int[] arExpected)
+    {
+      DoubleLinkedList actual = new DoubleLinkedList(actualAr);
+      DoubleLinkedList expected = new DoubleLinkedList(arExpected);
 
-            actual.AddByZeroIndex(value);
+      actual.AddByZeroIndex(value);
 
-            Assert.AreEqual(expected, actual);
-        }
+      Assert.AreEqual(expected, actual);
+    }
 
-        [TestCase(3, 5, new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 5, 4, 5 })]
-        [TestCase(0, 5, new int[] { 1, 2, 3, 4, 5 }, new int[] { 5, 1, 2, 3, 4, 5 })]
-        [TestCase(4, 5, new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 4, 5, 5 })]
-        [TestCase(0, 5, new int[] { }, new int[] { 5 })]
-        public void AddByIndexTest(int index, int value, int[] array, int[] expectedArray)
-        {
-            DoubleLinkedList actual = new DoubleLinkedList(array);
-            DoubleLinkedList expected = new DoubleLinkedList(expectedArray);
+    [TestCase(3, 5, new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 5, 4, 5 })]
+    [TestCase(0, 5, new int[] { 1, 2, 3, 4, 5 }, new int[] { 5, 1, 2, 3, 4, 5 })]
+    [TestCase(4, 5, new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 4, 5, 5 })]
+    [TestCase(0, 5, new int[] { }, new int[] { 5 })]
+    public void AddByIndexTest(int index, int value, int[] array, int[] expectedArray)
+    {
+      DoubleLinkedList actual = new DoubleLinkedList(array);
+      DoubleLinkedList expected = new DoubleLinkedList(expectedArray);
 
-            actual.AddByIndex(index, value);
+      actual.AddByIndex(index, value);
 
-            Assert.AreEqual(expected, actual);
-        }
+      Assert.AreEqual(expected, actual);
+    }
 
-        [TestCase(new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 4 })]
-        public void RemoveLastElementTest(int[] array, int[] expectedArray)
-        {
-            DoubleLinkedList actual = new DoubleLinkedList(array);
-            DoubleLinkedList expected = new DoubleLinkedList(expectedArray);
+    [TestCase(new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 4 })]
+    public void RemoveLastElementTest(int[] array, int[] expectedArray)
+    {
+      DoubleLinkedList actual = new DoubleLinkedList(array);
+      DoubleLinkedList expected = new DoubleLinkedList(expectedArray);
 
-            actual.RemoveLastElement();
+      actual.RemoveLastElement();
 
-            Assert.AreEqual(expected, actual);
-        }
+      Assert.AreEqual(expected, actual);
+    }
 
-        [TestCase(new int[] { 1, 2, 3, 4, 5 }, new int[] { 2, 3, 4, 5 })]
-        public void RemoveFirstTest(int[] array, int[] expectedArray)
-        {
-            DoubleLinkedList actual = new DoubleLinkedList(array);
-            DoubleLinkedList expected = new DoubleLinkedList(expectedArray);
+    [TestCase(new int[] { 1, 2, 3, 4, 5 }, new int[] { 2, 3, 4, 5 })]
+    public void RemoveFirstTest(int[] array, int[] expectedArray)
+    {
+      DoubleLinkedList actual = new DoubleLinkedList(array);
+      DoubleLinkedList expected = new DoubleLinkedList(expectedArray);
 
-            actual.RemoveFirst();
+      actual.RemoveFirst();
 
-            Assert.AreEqual(expected, actual);
-        }
+      Assert.AreEqual(expected, actual);
+    }
 
-    [TestCase(0,new int[] { 1, 2, 3, 4, 5 }, new int[] { 2, 3, 4, 5 })]
-    [TestCase(2, new int[] { 1, 2, 3, 4, 5 }, new int[] {1, 2, 4, 5 })]
-    [TestCase(4, new int[] { 1, 2, 3, 4, 5 }, new int[] {1, 2, 3, 4 })]
-    public void RemoveByIndexTest(int index,int[] array, int[] expectedArray)
+    [TestCase(0, new int[] { 1, 2, 3, 4, 5 }, new int[] { 2, 3, 4, 5 })]
+    [TestCase(2, new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 4, 5 })]
+    [TestCase(4, new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 4 })]
+    public void RemoveByIndexTest(int index, int[] array, int[] expectedArray)
     {
       DoubleLinkedList actual = new DoubleLinkedList(array);
       DoubleLinkedList expected = new DoubleLinkedList(expectedArray);
@@ -108,15 +108,66 @@ namespace List.Tests
       Assert.AreEqual(expected, actual);
     }
 
-    [TestCase(3, 3, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 1, 2,3, 6, 7, 8, 9 })]
+    [TestCase(3, 3, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 1, 2, 3, 7, 8, 9 })]
     [TestCase(4, 0, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 5, 6, 7, 8, 9 })]
-    [TestCase(2, 7, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 1, 2, 3, 4, 5,6,7 })]
+    [TestCase(2, 7, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 1, 2, 3, 4, 5, 6, 7 })]
     public void ClearByIndexXElementsTest(int x, int index, int[] array, int[] expectedArray)
     {
       DoubleLinkedList actual = new DoubleLinkedList(array);
       DoubleLinkedList expected = new DoubleLinkedList(expectedArray);
 
       actual.ClearByIndexXElements(x, index);
+
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestCase(new int[] { 1, 2, 3, 4, 5 }, new int[] { 5, 4, 3, 2, 1 })]
+    [TestCase(new int[] { 1, 4, 6, 2, 5, 4 }, new int[] { 4, 5, 2, 6, 4, 1 })]
+    public void ReverseTest(int[] array, int[] expectedAr)
+    {
+      DoubleLinkedList actual = new DoubleLinkedList(array);
+      DoubleLinkedList expected = new DoubleLinkedList(expectedAr);
+
+      actual.Revers();
+
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestCase(4, new int[] { 3, 4, 6, 4, 2, 5, 4 }, new int[] { 3, 6, 2, 5 }, 3)]
+    [TestCase(4, new int[] { 4, 4, 4 }, new int[] { }, 3)]
+    public void RemoveByValueAllTest(int value, int[] array, int[] expectedArray, int expectedCount)
+    {
+      LinkedList actual = new LinkedList(array);
+      LinkedList expected = new LinkedList(expectedArray);
+
+      int actualCount = actual.RemoveByValueAll(value);
+
+      Assert.AreEqual(expected, actual);
+      Assert.AreEqual(expectedCount, actualCount);
+    }
+
+    [TestCase(new int[] { 1, 2, 3, 4 }, new int[] { 5, 6, 7, 8, 9, 10 }, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })] // что тут должно быт на первом месте, тот массив в который мы вставляем или тот, который передаем?
+    public void AddArrayListInEndTest(int[] array, int[] insertArray, int[] expectedArray)
+    {
+      LinkedList actual = new LinkedList(array);
+      LinkedList expected = new LinkedList(expectedArray);
+      LinkedList insert = new LinkedList(insertArray);
+
+      actual.AddLinkedListInEnd(insert);
+
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestCase(2, new int[] { 1, 2, 3, 4 }, new int[] { 5, 6, 7, 8, 9, 10 }, new int[] { 1, 2, 5, 6, 7, 8, 9, 10, 3, 4 })]
+    [TestCase(3, new int[] { 1, 2, 3, 4 }, new int[] { 5, 6, 7, 8, 9, 10 }, new int[] { 1, 2, 3, 5, 6, 7, 8, 9, 10, 4 })]
+    [TestCase(3, new int[] { 1, 2, 3, 4, 5, 6, }, new int[] { 9, 9, 9 }, new int[] { 1, 2, 3, 9, 9, 9, 4, 5, 6, })]
+    public void AddArrayListByIndexTest(int index, int[] array, int[] insertArray, int[] expectedArray)
+    {
+      LinkedList actual = new LinkedList(array);
+      LinkedList expected = new LinkedList(expectedArray);
+      LinkedList insert = new LinkedList(insertArray);
+
+      actual.AddLinkedListByIndex(index, insert);
 
       Assert.AreEqual(expected, actual);
     }
--- List/DoubleLinkedList.cs
+++ List/DoubleLinkedList.cs
@@ -118,21 +118,30 @@ namespace List
 
     public void AddByIndex(int index, int value)
     {
-      if (index != 0)
+
+
+      if (index == 0)
       {
-        DoubleNode tmpDoubleNode = DoubleNodeByIndex(index - 1);
-        DoubleNode insertDoubleNode = new DoubleNode(value);
 
-        insertDoubleNode.Next = tmpDoubleNode.Next;
-        insertDoubleNode.Prev = tmpDoubleNode;
-        tmpDoubleNode.Next = insertDoubleNode;
-
-        Length++;
+        AddByZeroIndex(value);
+      }
+      else if (index == Length - 1)
+      {
+        Add(value);
       }
       else
       {
-        AddByZeroIndex(value);
+        DoubleNode firstDoubleNode = DoubleNodeByIndex(index - 1);
+        DoubleNode endDoubleNode = DoubleNodeByIndex(index);
+        DoubleNode insertDoubleNode = new DoubleNode(value);
+
+        insertDoubleNode.Next = endDoubleNode;
+        insertDoubleNode.Prev = firstDoubleNode;
+        firstDoubleNode.Next = insertDoubleNode;
+        endDoubleNode.Prev = insertDoubleNode;
+        Length++;
       }
+
     }
 
     public void RemoveLastElement()
@@ -145,7 +154,7 @@ namespace List
       DoubleNode tmpDoubleNode = DoubleNodeByIndex(Length - 2);
 
       tmpDoubleNode.Next = null;
-      _tail.Next = tmpDoubleNode;
+      _tail = tmpDoubleNode;
       Length--;
     }
 
@@ -174,10 +183,10 @@ namespace List
       }
       else
       {
-        tmpNode.Next = tmpNode.Next.Next;
-        tmpNode.Next.Prev = tmpNode.Prev;
+        DoubleNodeByIndex(index - 1).Next = DoubleNodeByIndex(index + 1);
+        DoubleNodeByIndex(index + 1).Prev = DoubleNodeByIndex(index - 1);
         Length--;
-      }      
+      }
     }
 
     public void RemoveXElementsByEnd(int x)
@@ -227,12 +236,247 @@ namespace List
 
       int tmpLength = Length - count;
 
-      while (Length!=tmpLength)
+      while (Length != tmpLength)
       {
         RemoveByIndex(index);
       }
     }
 
+    public int firstIndexByValue(int value)
+    {
+      int index = -1;
+      for (int i = 0; i < Length; i++)
+      {
+        if (value == DoubleNodeByIndex(i).Value)
+        {
+          index = i;
+          break;
+        }
+      }
+
+      return index;
+    }
+
+    public void Revers()
+    {
+      DoubleNode current = _root;
+      DoubleNode tmpDoubleNode = new DoubleNode(0);
+
+      while (current.Next != null)
+      {
+        tmpDoubleNode = current.Next;
+        current.Next = tmpDoubleNode.Next;
+        tmpDoubleNode.Next = _root;
+        _root = tmpDoubleNode;
+
+      }
+
+      DoubleLinkedList tmpdoubleLinkedList = new DoubleLinkedList(DoubleLinkedListTomMassInt());
+      _root = tmpdoubleLinkedList._root;
+      _tail = tmpdoubleLinkedList._tail;
+    }
+
+    private int[] DoubleLinkedListTomMassInt()
+    {
+      int[] tmpArray = new int[Length];
+      for (int i = 0; i < Length; i++)
+      {
+        tmpArray[i]=DoubleNodeByIndex(i).Value;
+      }
+      return tmpArray;
+    }
+
+    public int MaxValue()
+    {
+      if (Length == 0)
+      {
+        throw new ArgumentNullException("лист не имеет элементов");
+      }
+
+      int maxValue = DoubleNodeByIndex(0).Value;
+
+      for (int i = 1; i < Length; i++)
+      {
+        if (maxValue < DoubleNodeByIndex(i).Value)
+        {
+          maxValue = DoubleNodeByIndex(i).Value;
+        }
+      }
+
+      return maxValue;
+    }
+
+    public int MinValue()
+    {
+      if (Length == 0)
+      {
+        throw new ArgumentNullException("лист не имеет элементов");
+      }
+
+      int minValue = DoubleNodeByIndex(0).Value;
+
+      for (int i = 1; i < Length; i++)
+      {
+        if (minValue > DoubleNodeByIndex(i).Value)
+        {
+          minValue = DoubleNodeByIndex(i).Value;
+        }
+      }
+
+      return minValue;
+    }
+
+    public int IndexOfMaxValue()
+    {
+      if (Length == 0)
+      {
+        throw new ArgumentNullException("лист не имеет элементов");
+      }
+
+      int maxValue = DoubleNodeByIndex(0).Value;
+      int tmpindex = 0;
+
+      for (int i = 0; i < Length; i++)
+      {
+        if (maxValue < DoubleNodeByIndex(i).Value)
+        {
+          maxValue = DoubleNodeByIndex(i).Value;
+          tmpindex = i;
+        }
+      }
+
+      return tmpindex;
+    }
+
+    public int IndexOfMinValue()
+    {
+      if (Length == 0)
+      {
+        throw new ArgumentNullException("лист не имеет элементов");
+      }
+
+      int minValue = DoubleNodeByIndex(0).Value;
+
+      for (int i = 0; i < Length; i++)
+      {
+        if (minValue > DoubleNodeByIndex(i).Value)
+        {
+          minValue = i;
+        }
+      }
+
+      return minValue;
+    }
+
+    public void SortAscending()
+    {
+      for (int i = 1; i < Length; i++)
+      {
+        for (int j = 0; j < Length - i; j++)
+        {
+          int tmp;
+
+          if (DoubleNodeByIndex(j + 1).Value < DoubleNodeByIndex(j).Value)
+          {
+            tmp = DoubleNodeByIndex(j).Value;
+            DoubleNodeByIndex(j).Value = DoubleNodeByIndex(j + 1).Value;
+            DoubleNodeByIndex(j + 1).Value = tmp;
+          }
+        }
+      }
+    }
+
+    public void SortDescending()
+    {
+      for (int i = 1; i < Length; i++)
+      {
+        for (int j = 0; j < Length - i; j++)
+        {
+          int tmp;
+
+          if (DoubleNodeByIndex(j + 1).Value > DoubleNodeByIndex(j).Value)
+          {
+            tmp = DoubleNodeByIndex(j).Value;
+            DoubleNodeByIndex(j).Value = DoubleNodeByIndex(j + 1).Value;
+            DoubleNodeByIndex(j + 1).Value = tmp;
+          }
+        }
+      }
+    }
+
+    public int RemoveByValueFisrt(int value)
+    {
+      if (Length == 0)
+      {
+        throw new ArgumentNullException("лист не имеет элементов");
+      }
+
+      int index = -1;
+      for (int i = 1; i < Length; i++)
+      {
+        if (DoubleNodeByIndex(i).Value == value)
+        {
+          RemoveByIndex(i);
+
+          return index;
+        }
+
+      }
+      return index;
+    }
+
+    public int RemoveByValueAll(int value)
+    {
+      if (Length == 0)
+      {
+        throw new ArgumentNullException("лист не имеет элементов");
+      }
+
+      int count = 0;
+
+      for (int i = 0; i < Length; i++)
+      {
+        if (DoubleNodeByIndex(i).Value == value)
+        {
+          RemoveByIndex(i);
+          count++;
+          i--;
+        }
+
+      }
+
+      return count;
+    }
+
+    public void AddLinkedListInEnd(DoubleLinkedList insertLinkedList)
+    {
+      AddLinkedListByIndex(Length, insertLinkedList);
+    }
+
+    public void AddArrayListByZeroIndex(DoubleLinkedList insertLinkedList)
+    {
+      AddLinkedListByIndex(0, insertLinkedList);
+    }
+
+    public void AddLinkedListByIndex(int index, DoubleLinkedList insertLinkedList)
+    {
+      Length += insertLinkedList.Length;
+      DoubleNode tmpNode = DoubleNodeByIndex(index);
+      int[] tmpArray = new int[Length];
+      DoubleNodeByIndex(index - 1).Next = insertLinkedList._root;
+
+      DoubleNodeByIndex(index + insertLinkedList.Length - 1).Next = tmpNode;
+
+      for (int i = 0; i < Length; i++)
+      {
+        tmpArray[i] = DoubleNodeByIndex(i).Value;
+      }
+      DoubleLinkedList tmpLinkedList = new DoubleLinkedList(tmpArray);
+
+      _root = tmpLinkedList._root;
+      _tail = tmpLinkedList._tail;
+
+    }
     public override string ToString()
     {
       if (Length != 0)
@@ -276,45 +520,48 @@ namespace List
         currentThis = currentThis.Next;
       }
 
+      currentThis = _tail;
+      currentList = list._tail;
+
+      while (!(currentThis is null))
+      {
+        if (currentThis.Value != currentList.Value)
+        {
+          return false;
+        }
+
+        currentList = currentList.Prev;
+        currentThis = currentThis.Prev;
+      }
       return true;
     }
+
     private DoubleNode DoubleNodeByIndex(int index)
     {
       if (index > Length - 1)
       {
         throw new ArgumentOutOfRangeException("netu indexa");
       }
-
-      DoubleNode firstDoubleNode = _root;
-      DoubleNode enddoubleNode = _tail;
+      DoubleNode tmpDoubleNode = new DoubleNode(0);
       bool indexMoreHalf = index >= Length / 2;
-      int tmpIndex = Length - index;
 
       if (indexMoreHalf)
       {
-        index = tmpIndex;
-      }
-
-      for (int i = 0; i < index - 1; i++)
-      {
-        if (indexMoreHalf)
+        tmpDoubleNode = _tail;
+        for (int i = 0; i < Length - index - 1; i++)
         {
-          enddoubleNode = enddoubleNode.Prev;
+          tmpDoubleNode = tmpDoubleNode.Prev;
         }
-        else
-        {
-          firstDoubleNode = firstDoubleNode.Next;
-        }
-      }
-
-      if (indexMoreHalf)
-      {
-        return enddoubleNode;
       }
       else
       {
-        return firstDoubleNode;
+        tmpDoubleNode = _root;
+        for (int i = 0; i < index; i++)
+        {
+          tmpDoubleNode = tmpDoubleNode.Next;
+        }
       }
+      return tmpDoubleNode;
     }
   }
 }
